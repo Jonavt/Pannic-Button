f0275b6b0095d0e3959580d42562608704d9f2f6
Arturo <adiaz0511@hotmail.com>
2023-03-07 17:26:18 -0600

    Mapa

--- Panic Button/ContentView.swift
+++ Panic Button/ContentView.swift
@@ -6,11 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.334_900,
+                                       longitude: -122.009_020),
+        latitudinalMeters: 750,
+        longitudinalMeters: 750
+    )
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Map(coordinateRegion: $region)
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
