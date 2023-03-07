7d1e1106bbfe7f1bea3f4e62c983d040c4611c66
Jonathan <jonathan.valenciato@udlap.mx>
2023-03-07 17:20:35 -0600

    Initial Commit

---
+++ Panic Button/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Panic Button
+//
+//  Created by UDLAP14 on 07/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Panic Button/Panic_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Panic_ButtonApp.swift
+//  Panic Button
+//
+//  Created by UDLAP14 on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Panic_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
