f8093e5469b6dbf6a896edbabe30bbcd8e83eb08
Arturo <adiaz0511@hotmail.com>
2023-03-09 17:17:17 -0600

    Cambio de diseño

--- Panic Button/ContentView.swift
+++ Panic Button/ContentView.swift
@@ -9,24 +9,12 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 19.0549,
-                                       longitude: -98.2845),
-        latitudinalMeters: 750,
-        longitudinalMeters: 750
-    )
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Map(coordinateRegion: $region)
-                .edgesIgnoringSafeArea(.all)
+        VStack(alignment: .center, spacing: 10) {
             
-            .controlSize(.large)
-            .buttonStyle(.borderedProminent)
-            .sheet(isPresented: .constant(true)) {
-                HomeView()
-                    .presentationDetents([.fraction(0.4)])
-            }
+            HomeView()
+            
         }
     }
 }
--- Panic Button/HomeView.swift
+++ Panic Button/HomeView.swift
@@ -6,8 +6,16 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct HomeView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 19.0549,
+                                       longitude: -98.2845),
+        latitudinalMeters: 750,
+        longitudinalMeters: 750
+    )
+
     var body: some View {
         VStack(alignment: .leading, spacing: 25) {
             HStack {
@@ -26,6 +34,12 @@ struct HomeView: View {
                 .background(Color.secondary.opacity(0.2))
                 .clipShape(Circle())
             }
+            
+            Map(coordinateRegion: $region)
+                .frame(minHeight: 400)
+                .cornerRadius(20)
+
+//            Spacer()
             
             Button {
                 
@@ -106,6 +120,7 @@ struct HomeView: View {
                 .accentColor(.secondary)
                 
             }
+            
             
         }
         .padding()
